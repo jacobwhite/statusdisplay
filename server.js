37dc4542e766aa48c9c423a1859b9ce9f9234706
Jacob <me@jacobwhite.us>
2020-05-09 19:21:27 -0700

    De-galaxy-brained some stuff, removed some debug logging.

--- server.js
+++ server.js
@@ -12,134 +12,131 @@ const querystring = require('querystring');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  //console.log(req.url);
   let parsedURL = url.parse(req.url);
-  //console.log(parsedURL.pathname);
   
-  switch(parsedURL.pathname){
-    case "/":
-      let queryParameters = querystring.parse(url.parse(req.url).query);
-	  console.log("query: ", queryParameters['color'], queryParameters['status']);
-	  if(queryParameters['status'] == undefined && queryParameters['color'] == undefined){
-		fs.readFile("html/index.html", function(err, data){
-		  var mimetype="text/html";
-		  res.setHeader("Access-Control-Allow-Origin", "*");
-		  res.writeHead(200, {'Content-Type': mimetype});
-		  res.write(data);
-		  res.end();
-		});
-	  }
-	  else {
-		  res.statusCode = 200;
-		  res.setHeader('Content-Type', 'text/plain');
-		  global.status = queryParameters['status'];
-		  global.color = queryParameters['color'];
-		  broadcastStatus();
-		  res.end('{"result":"success","status":"'+status+'","color":"'+color+'"}');
-	}	
-    break;
-	default:
-	fs.readFile("html"+parsedURL.pathname, function(err, data){
-	var mimetype="text/html";
-	let extension = parsedURL.pathname.substr(parsedURL.pathname.lastIndexOf('.') + 1);
-		if(err){
-			res.writeHead(404);
-			res.write("not found");
-		}
-		else {
-			//console.log("extension",extension);
-		  switch(extension){
-			case "odf":
-			  mimetype="font/opentype";
-			  break;
-			case "mp3":
-				mimetype="audio/mpeg";
-				break;
-			case "html":
-				mimetype="text/html";
-				break;
-			case "css":
-				mimetype="text/css";
-				break;
-			case "js":
-				mimetype="text/javascript";
-				break;
-		  }
-		  res.setHeader("Access-Control-Allow-Origin", "*");
-		  res.writeHead(200, {'Content-Type': mimetype});
-		  res.write(data);
-		  res.end();
-		}
-	});
+  if(parsedURL.pathname == "/"){
+    let queryParameters = querystring.parse(url.parse(req.url).query);
+    
+    if(queryParameters['status'] != undefined || queryParameters['color'] != undefined){
+      global.status = queryParameters['status'];
+      global.color = queryParameters['color'];
+      broadcastStatus();
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('{"result":"success","status":"'+status+'","color":"'+color+'"}');
+    }
+    else {
+      serveFile(res,"html/index.html");
+      }  
+	}
+	else {
+    serveFile(res, "html"+parsedURL.pathname);
   }
 });
 
+function serveFile(res, file){
+  console.log("serving file:", file);
+  fs.readFile(file, function(err, data){
+    var mimetype="text/html";
+    let extension = file.substr(file.lastIndexOf('.') + 1);
+    if(err){
+      res.writeHead(404);
+      res.write("not found");
+    }
+    else {
+      switch(extension){
+      case "odf":
+        mimetype="font/opentype";
+        break;
+      case "mp3":
+        mimetype="audio/mpeg";
+        break;
+      case "html":
+        mimetype="text/html";
+        break;
+      case "css":
+        mimetype="text/css";
+        break;
+      case "js":
+        mimetype="text/javascript";
+        break;
+      }
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.writeHead(200, {'Content-Type': mimetype});
+      res.write(data);
+      res.end();
+    }
+  });
+}
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`HTTP Server running at http://${hostname}:${port}/`);
 });
 
 //Web Socket Stuff
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 })
-console.log("Ready"); 
+console.log("Websocket Ready"); 
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log("received:", message)
-	var msg = JSON.parse(message);
-	
-	//If message is a status update
-	if(msg.type == "status"){
-		console.log(msg.type, "updated to", msg.status);
-		global.status = msg.status;
-		global.color = msg.color;
+    var msg = JSON.parse(message);
+  
+    //If message is a status update
+    if(msg.type == "status"){
+      console.log(msg.type, "updated to", msg.status);
+      global.status = msg.status;
+      global.color = msg.color;
 
-		//respond to update
-		var response = {
-			type: "response",
-			value: "success",
-			status: global.status,
-			color: global.color
+      //respond to update
+      var response = {
+        type: "response",
+        value: "success",
+        status: global.status,
+        color: global.color
+      }
+      ws.send(JSON.stringify(response));
+    
+      //Broadcast update to all displays
+      broadcastStatus();
+    }
 
-		}
-		ws.send(JSON.stringify(response));
-		
-		//Broadcast update to all displays
-		broadcastStatus();
-	}
+    if(msg.type == "getStatus"){
+      var broadcastMessage = {
+        type: "getStatus"
+      }
+      wss.broadcast(JSON.stringify(broadcastMessage));
+    }
 
-	if(msg.type == "getStatus"){
-		var broadcastMessage = {
-			type: "getStatus"
-		}
-		wss.broadcast(JSON.stringify(broadcastMessage));
-	}
+    if(msg.type == "displayStatus"){
+      wss.broadcast(JSON.stringify(msg));
+    }
 
-	if(msg.type == "displayStatus"){
-		wss.broadcast(JSON.stringify(msg));
-	}
-	if(msg.type == "register"){
-		var message = {
-			type: "status",
-			status: global.status,
-			color: global.color
-		}
-		console.log("responding to register: ", message);
-		ws.send(JSON.stringify(message));
-	}
+    if(msg.type == "register"){
+      var message = {
+        type: "status",
+        status: global.status,
+        color: global.color
+      }
+      console.log("responding to register: ", message);
+      ws.send(JSON.stringify(message));
+    }
   })
 })
+
 wss.broadcast = function(msg) {
-	console.log("broadcast: ", msg);
-	wss.clients.forEach(function each(client){
-		console.log("    sending", msg);
-		client.send(msg);
-	});
+  console.log("broadcast: ", msg);
+  wss.clients.forEach(function each(client){
+    console.log("    sending", msg);
+    client.send(msg);
+  });
 };
+
 function broadcastStatus(){
-	var broadcastMessage = {
-		type: "status",
-		status: global.status,
-		color: global.color
-	}
-wss.broadcast(JSON.stringify(broadcastMessage));
+  var broadcastMessage = {
+    type: "status",
+    status: global.status,
+    color: global.color
+  }
+  wss.broadcast(JSON.stringify(broadcastMessage));
 }
